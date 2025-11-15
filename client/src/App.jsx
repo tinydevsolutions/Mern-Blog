60392bb971c01c57acbc469e2ea3022e6ab8ec4f
tinydevsolutions <tinydevsolutions@gmail.com>
2025-11-15 21:42:33 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Project from "./pages/Project";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 const App = () => {
   return (
-    <div>
-      <h1 className="text-red-500">App</h1>
-    </div>
+    <>
+      <header></header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </main>
+      <footer></footer>
+    </>
   );
 };
 
